645623fd85ded070f88cd8445e23f6f0128a9d4f
Allysom Oliveira <allysomoliveira7@gmail.com>
2020-06-10 14:35:29 -0300

    Finalizado video aula sobre como manuseiar Datas e Horas em PHP.

--- Data.php
+++ Data.php
@@ -5,4 +5,28 @@
     PHP version 7.4
 */
 //Trabalhando com datas
+//echo date('d/m/Y'). PHP_EOL;
+//Metodo de date
+$data = new DateTime();
+echo 'Date: '. $data -> format('d/m/y').PHP_EOL.'Hora: '.$data -> format('H:i:s').PHP_EOL; 
+//Metodo DateTime
+$data =  new DateTime();
+/*
+    -> P    Representação de período: vem antes de dia, mês, ano e semana
+    Y       Anos
+    M       Meses
+    D       Dias
+    W       Semana
+    ->T     Representação de tempo: vem antes de hora, minuot e segundo;
+    H       Horas;
+    M       Minutos;
+    S       Segundos;
+ */
+    //Criando intervalo 
+ $intervalo - new DateInterval('PT5M'); //Adciona um peíodo de 5 minutos.
+    //Adicionando ...
+ $data-> add($intervalo);
+
+   //IMPRESSÂO 
+ var_dump($data);
 ?>
